7a826020442014a9c25ac44a9a1b627205a20255
Laboratorio Unidade Fatima <labunivas@univas.edu.br>
2024-03-04 19:34:06 -0300

    add atividade

---
+++ tiposdadosav-calculadora_v1/src/br/calculadora/Calculadora.java
@@ -0,0 +1,18 @@
+package br.calculadora;
+
+public class Calculadora{
+	public int calcular(int valor) throws Exception{
+		if(valor > 1000) {
+			return valor - (int)(valor * 0.3);
+		}
+		else if(valor > 500) {
+			return valor - (int)(valor * 0.25);
+		}
+		else if(valor > 0) {
+			return valor - (int)(valor * 0.1);
+		}
+		else {
+			throw new Exception("O valor deve ser positivo");
+		}
+	}
+}
